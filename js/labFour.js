9c5e8f50ff8bbde14adc5444f6d6140e3a9e84ca
Alison Mohr <alison@newbo.co>
2020-06-21 22:35:54 -0500

    added functions to questions

--- js/labFour.js
+++ js/labFour.js
@@ -11,102 +11,112 @@ alert('Hello, ' + userName + '! Welcome to my site.');
 
 
 //prompt our user for five yes or no questions
-var favoriteFood = prompt('Is ice cream my favorite food?');
-console.log(favoriteFood);
 
-var lowerCaseFood = favoriteFood.toLowerCase();
-console.log(lowerCaseFood);
+function questionOne(){
+  var favoriteFood = prompt('Is ice cream my favorite food?');
+  console.log(favoriteFood);
 
-if(lowerCaseFood === 'yes' || lowerCaseFood === 'y') {
+  var lowerCaseFood = favoriteFood.toLowerCase();
+  console.log(lowerCaseFood);
+
+  if(lowerCaseFood === 'yes' || lowerCaseFood === 'y') {
   //console.log('You are correct. Whitey\'s Ice Cream in Davenport makes the best ice cream in the world.');
-  alert('You are correct. Whitey\'s Ice Cream in Davenport makes the best ice cream in the world.');
-  userPoints +=1;
-} else if(lowerCaseFood === 'no'|| lowerCaseFood === 'n') {
+    alert('You are correct. Whitey\'s Ice Cream in Davenport makes the best ice cream in the world.');
+    userPoints +=1;
+  } else if(lowerCaseFood === 'no'|| lowerCaseFood === 'n') {
   //console.log ('Incorrect, I love ice cream.');
-  alert('Incorrect, I love ice cream.');
-}
-else {
+    alert('Incorrect, I love ice cream.');
+  }
+  else {
   //console.log ('Please remember to respond using yes or no.');
-  alert('Please remember to respond using yes or no.');
+    alert('Please remember to respond using yes or no.');
+  }
 }
 
-var myKids = prompt('Do I have two children?');
-console.log(myKids);
+function questionTwo(){
+  var myKids = prompt('Do I have two children?');
+  console.log(myKids);
 
-var lowerCaseKids = myKids.toLowerCase();
-console.log(lowerCaseKids);
+  var lowerCaseKids = myKids.toLowerCase();
+  console.log(lowerCaseKids);
 
-if(lowerCaseKids === 'yes' || lowerCaseKids === 'y') {
+  if(lowerCaseKids === 'yes' || lowerCaseKids === 'y') {
   //console.log('You are correct. I have a two and four year old.');
-  alert('You are correct. I have a two and four year old.');
-  userPoints +=1;
-} else if(lowerCaseKids === 'no'|| lowerCaseKids === 'n') {
+    alert('You are correct. I have a two and four year old.');
+    userPoints +=1;
+  } else if(lowerCaseKids === 'no'|| lowerCaseKids === 'n') {
   //console.log ('Incorrect, I have a son and daughter.');
-  alert('Incorrect, I have a son and daughter.');
-}
-else {
+    alert('Incorrect, I have a son and daughter.');
+  }
+  else {
   //console.log ('Please remember to respond using yes or no.');
-  alert('Please remember to respond using yes or no.');
+    alert('Please remember to respond using yes or no.');
+  }
 }
 
-var footballTeam = prompt('Are the Greenbay Packers the best team in the NFL?');
-console.log(footballTeam);
+function questionThree(){
+  var footballTeam = prompt('Are the Greenbay Packers the best team in the NFL?');
+  console.log(footballTeam);
 
-var lowerCaseFootball = footballTeam.toLowerCase();
-console.log(lowerCaseFootball);
+  var lowerCaseFootball = footballTeam.toLowerCase();
+  console.log(lowerCaseFootball);
 
-if(lowerCaseFootball === 'yes' || lowerCaseFootball === 'y') {
+  if(lowerCaseFootball === 'yes' || lowerCaseFootball === 'y') {
   //console.log('You are correct. The Packers rock.');
-  alert('You are correct. The Packers rock.');
-  userPoints +=1;
-} else if(lowerCaseFootball === 'no'|| lowerCaseFootball === 'n') {
+    alert('You are correct. The Packers rock.');
+    userPoints +=1;
+  } else if(lowerCaseFootball === 'no'|| lowerCaseFootball === 'n') {
   //console.log ('I\'m afraid you are confused.');
-  alert('I\'m afraid you are confused.');
-}
-else {
+    alert('I\'m afraid you are confused.');
+  }
+  else {
   //console.log ('Please remember to respond using yes or no.');
-  alert('Please remember to respond using yes or no.');
+    alert('Please remember to respond using yes or no.');
+  }
 }
 
-var myHobby = prompt('Do I enjoy distance running?');
-console.log(myHobby);
+function questionFour(){
+  var myHobby = prompt('Do I enjoy distance running?');
+  console.log(myHobby);
 
-var lowerCaseHobby = myHobby.toLowerCase();
-console.log(lowerCaseHobby);
+  var lowerCaseHobby = myHobby.toLowerCase();
+  console.log(lowerCaseHobby);
 
-if(lowerCaseHobby === 'yes' || lowerCaseHobby === 'y') {
+  if(lowerCaseHobby === 'yes' || lowerCaseHobby === 'y') {
   //console.log('You are correct.');
-  alert('You are correct.');
-  userPoints +=1;
-} else if(lowerCaseHobby === 'no'|| lowerCaseHobby === 'n') {
+    alert('You are correct.');
+    userPoints +=1;
+  } else if(lowerCaseHobby === 'no'|| lowerCaseHobby === 'n') {
   //console.log ('Incorrect, I love to run.');
-  alert('Incorrect, I love to run.');
-}
-else {
+    alert('Incorrect, I love to run.');
+  }
+  else {
   //console.log ('Please remember to respond using yes or no.');
-  alert('Please remember to respond using yes or no.');
+    alert('Please remember to respond using yes or no.');
+  }
 }
 
+function questionFive(){
+  var myTown = prompt ('Do I live in Cedar Rapids?');
+  console.log(myTown);
 
-var myTown = prompt ('Do I live in Cedar Rapids?');
-console.log(myTown);
+  //convert answers to lowerCase
+  var lowerCaseTown = myTown.toLowerCase();
+  console.log(lowerCaseTown);
 
-//convert answers to lowerCase
-var lowerCaseTown = myTown.toLowerCase();
-console.log(lowerCaseTown);
-
-//validate the input using conditional logic using strings
-if(lowerCaseTown === 'yes' || lowerCaseTown === 'y') {
+  //validate the input using conditional logic using strings
+  if(lowerCaseTown === 'yes' || lowerCaseTown === 'y') {
   //console.log('You are incorrect. I live in Central Iowa.');
-  alert('You are incorrect. I live in Central Iowa.');
-} else if(lowerCaseTown === 'no'|| lowerCaseTown === 'n') {
+    alert('You are incorrect. I live in Central Iowa.');
+  } else if(lowerCaseTown === 'no'|| lowerCaseTown === 'n') {
   //console.log ('Correct, I live in the home of Adventureland.');
-  alert('Correct, I live in the home of Adventureland.');
-  userPoints += 1;
-}
-else {
+    alert('Correct, I live in the home of Adventureland.');
+    userPoints += 1;
+  }
+  else {
   //console.log ('Please remember to respond using yes or no.');
-  alert('Please remember to respond using yes or no.');
+    alert('Please remember to respond using yes or no.');
+  }
 }
 
 //6th Question - Question that require numeric input. Alert indicates if it's too high or too low.
@@ -178,6 +188,12 @@ function questionSeven(){
   }
 }
 
+
+questionOne();
+questionTwo();
+questionThree();
+questionFour();
+questionFive();
 questionSix();
 questionSeven();
 
